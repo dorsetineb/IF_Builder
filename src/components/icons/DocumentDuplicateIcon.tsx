02dcbf91b74dca24aea215d03678b18168a4cf79
dorsetineb <cotzbenites@gmail.com>
2026-01-30 17:02:29 -0300

    otimização do editor

--- src/components/icons/DocumentDuplicateIcon.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// Icon removed as per user request.
-export const DocumentDuplicateIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  null
-);
--- src/components/icons/SparklesIcon.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// Ícone removido pois a funcionalidade de IA Generativa foi desativada.
-export const SparklesIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg {...props} />
-);
